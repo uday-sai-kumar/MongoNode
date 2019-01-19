eb18a822faf8eb459b482ffae8741130336686f7
UdaySaiKumar <udaysaikumar1919@gmail.com>
2019-01-19 18:45:17 +0530

    hello usk

---
+++ connect_to_mlab.js
@@ -0,0 +1,25 @@
+var mon=require('mongodb').MongoClient;
+//console.log(mon);
+var url="mongodb://localhost:27017/";
+mon.connect(url,{useNewUrlParser:true},(err,client)=>{
+  if(err)
+return  console.log("connected unsuccessfull");
+console.log("connected successfull")
+  var db=client.db('wow');
+  db.collection("wow1").find().toArray().then((array)=>{
+    console.log(`wow +${JSON.stringify(array,null,2)}`);
+  },(err)=>{
+    console.log("failed",err);
+  });
+  // db.collection('wow1').insertOne({
+  //   name:"uday is a good boy",
+  //   age:"22"
+  // },(err,result)=>{
+  //   if(err){
+  //     return console.log("unable to insert",err);
+  //   }
+  //   console.log(result);
+  //   console.log(JSON.stringify(result.ops,undefined,10));
+  // });
+  client.close();
+});
